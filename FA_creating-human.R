948bc3516a2ac804db3ddcd49d553448405c1a08
JoaWe <33264029+JoaWe@users.noreply.github.com>
2017-12-13 22:51:41 +0200

    Preparing the markdown-file for the report
    File saved into the IODS-final folder, included and knitted on the index.Rmd file

--- FA_creating-human.R
+++ FA_creating-human.R
@@ -3,3 +3,16 @@
 
 # Datawrangling-Script for the IODS-Final-Assignment 
 
+# Choosing and loading the datasets "human development (hd)" and "gender inequality index (gii)":
+
+hd <- read.csv("http://s3.amazonaws.com/assets.datacamp.com/production/course_2218/datasets/human_development.csv", stringsAsFactors = F)
+gii <- read.csv("http://s3.amazonaws.com/assets.datacamp.com/production/course_2218/datasets/gender_inequality.csv", stringsAsFactors = F, na.strings = "..")
+
+# taking a look at their structures and dimensions:
+
+str(hd)
+dim(hd)
+
+str(gii)
+dim(gii)
+
